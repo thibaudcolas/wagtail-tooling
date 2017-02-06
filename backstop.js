559df46243c24ff826109bb553ea5a94a7aae19e
Thibaud Colas <thibaudcolas@gmail.com>
2017-02-06 12:08:20 +0200

    Tweak test suites

--- backstop.js
+++ backstop.js
@@ -1,72 +1,71 @@
 require('dotenv').config();
 
+if (!process.env.WAGTAIL_SESSIONID) {
+  throw new ReferenceError('Environment variable WAGTAIL_SESSIONID is not defined.');
+}
+
 const domain = 'localhost:8000';
 
 const composeScenario = (...args) => Object.assign({}, ...args);
 const contentOnly = composeScenario.bind(null, { selectors : [ '.content-wrapper' ]});
-const clickSelector = (clickSelector) => composeScenario.bind(null, { clickSelector, onReadyScript: 'clickSelector.js', delay: 2000 });
+const clickSelector = (clickSelector) => composeScenario.bind(null, { clickSelector, onReadyScript: 'clickSelector.js' });
 
 const scenarios = [
-  // { path: '/login', onBeforeScript: null },
-  // { path: '/', label: 'No JS', onBeforeScript: 'disableJS.js' },
-  // { path: '/', selectors: [ '.nav-wrapper', '.content-wrapper' ]},
-  // TODO CHECK
-  clickSelector('.dl-trigger')({ path: '/', label: 'Explorer', delay: 5000 }),
+  { path: '/login', onBeforeScript: null },
+  { path: '/', label: 'No JS', onBeforeScript: 'disableJS.js' },
+  { path: '/', selectors: [ '.nav-wrapper', '.content-wrapper' ]},
+  clickSelector('.dl-trigger')({ path: '/', label: 'Explorer' }),
   clickSelector('.submenu-trigger')({ path: '/', label: 'Settings' }),
   clickSelector('#account-settings')({ path: '/', selectors: [ '.nav-wrapper' ], label: 'Account settings' }),
   clickSelector('#nav-toggle')({ path: '/', label: 'Mobile nav' }),
-  // contentOnly({ path: '/pages/' }),
-  // contentOnly({ path: '/pages/search/?q=index' }),
-  // contentOnly({ path: '/pages/5/' }),
-  // contentOnly({ path: '/pages/5/edit' }),
+  contentOnly({ path: '/pages/' }),
+  contentOnly({ path: '/pages/search/?q=index' }),
+  contentOnly({ path: '/pages/5/' }),
+  contentOnly({ path: '/pages/5/edit' }),
   contentOnly(clickSelector('[href*="promote"]')({ path: '/pages/5/edit/#promote' })),
   contentOnly(clickSelector('[href*="settings"]')({ path: '/pages/5/edit/#settings' })),
   // TODO Test date UI on settings tab.
   contentOnly(clickSelector('[href*="privacy"]')({ path: '/pages/5/edit/#privacy' })),
-  // TODO CHECK
-  contentOnly(clickSelector('.dropdown-toggle')({ path: '/pages/5/edit/#dropdown-toggle' })),
-  // TODO CHECK
+  // TODO Only works well once every two clicks (toggle).
+  contentOnly(clickSelector('.dropdown-toggle')({ path: '/pages/5/edit/#dropdown-toggle', label: 'Page dropdown' })),
   contentOnly(clickSelector('.page-chooser .chosen .action-choose')({ path: '/pages/5/edit/', label: 'Page chooser' })),
-  // TODO CHECK
   contentOnly(clickSelector('.document-chooser .unchosen .action-choose')({ path: '/pages/5/edit/', label: 'Document chooser' })),
-  // contentOnly({ path: '/pages/5/revisions/' }),
-  // contentOnly({ path: '/pages/5/unpublish/' }),
-  // contentOnly({ path: '/pages/5/delete/' }),
-  // contentOnly({ path: '/pages/5/copy/' }),
-  // contentOnly({ path: '/pages/5/move/' }),
-  // contentOnly({ path: '/pages/5/add_subpage/' }),
+  contentOnly({ path: '/pages/5/revisions/' }),
+  contentOnly({ path: '/pages/5/unpublish/' }),
+  contentOnly({ path: '/pages/5/delete/' }),
+  contentOnly({ path: '/pages/5/copy/' }),
+  contentOnly({ path: '/pages/5/move/' }),
+  contentOnly({ path: '/pages/5/add_subpage/' }),
   // // Does not work on my instance.
   // // contentOnly({ path: '/images/'}),
-  // contentOnly({ path: '/images/add/'}),
-  // contentOnly({ path: '/documents/' }),
-  // contentOnly({ path: '/documents/edit/3/' }),
-  // contentOnly({ path: '/documents/multiple/add/' }),
-  // clickSelector('.submenu-trigger')({ path: '/' }),
-  // contentOnly({ path: '/snippets/' }),
-  // contentOnly({ path: '/snippets/demo/advert/' }),
-  // contentOnly({ path: '/snippets/demo/advert/1/' }),
-  // contentOnly({ path: '/snippets/demo/advert/add/' }),
-  // contentOnly({ path: '/users/' }),
-  // contentOnly({ path: '/users/1/' }),
-  // contentOnly({ path: '/users/add/' }),
-  // contentOnly(clickSelector('[href*="roles"]')({ path: '/users/add/#roles' })),
-  // contentOnly({ path: '/groups/' }),
-  // contentOnly({ path: '/groups/1/' }),
-  // contentOnly({ path: '/groups/add/' }),
-  // contentOnly({ path: '/sites/' }),
-  // contentOnly({ path: '/sites/1/' }),
-  // contentOnly({ path: '/sites/add/' }),
-  // contentOnly({ path: '/collections/' }),
-  // contentOnly({ path: '/collections/add/' }),
-  // contentOnly({ path: '/redirects/' }),
-  // contentOnly({ path: '/redirects/add/' }),
-  // contentOnly({ path: '/searchpicks/' }),
-  // contentOnly({ path: '/searchpicks/add/' }),
-  // TODO CHECK
+  contentOnly({ path: '/images/add/'}),
+  contentOnly({ path: '/documents/' }),
+  contentOnly({ path: '/documents/edit/3/' }),
+  contentOnly({ path: '/documents/multiple/add/' }),
+  contentOnly({ path: '/snippets/' }),
+  contentOnly({ path: '/snippets/demo/advert/' }),
+  contentOnly({ path: '/snippets/demo/advert/1/' }),
+  contentOnly({ path: '/snippets/demo/advert/add/' }),
+  contentOnly({ path: '/users/' }),
+  contentOnly({ path: '/users/1/' }),
+  contentOnly({ path: '/users/add/' }),
+  contentOnly(clickSelector('[href*="roles"]')({ path: '/users/add/#roles' })),
+  contentOnly({ path: '/groups/' }),
+  contentOnly({ path: '/groups/1/' }),
+  contentOnly({ path: '/groups/add/' }),
+  contentOnly({ path: '/sites/' }),
+  contentOnly({ path: '/sites/1/' }),
+  contentOnly({ path: '/sites/add/' }),
+  contentOnly({ path: '/collections/' }),
+  contentOnly({ path: '/collections/add/' }),
+  contentOnly({ path: '/redirects/' }),
+  contentOnly({ path: '/redirects/add/' }),
+  contentOnly({ path: '/searchpicks/' }),
+  contentOnly({ path: '/searchpicks/add/' }),
   contentOnly(clickSelector('#id_query_string-chooser')({ path: '/searchpicks/add/', label: 'Search term chooser' })),
-  // contentOnly({ path: '/account/' }),
-  // contentOnly({ path: '/account/change_password/' }),
-  // contentOnly({ path: '/account/notification_preferences/' }),
+  contentOnly({ path: '/account/' }),
+  contentOnly({ path: '/account/change_password/' }),
+  contentOnly({ path: '/account/notification_preferences/' }),
   // Revokes the session cookie, which is annoying when testing.
   // { path: '/admin/logout' },
   // Loading spinner always makes the test fail.
@@ -84,12 +83,6 @@ const scenarios = [
     onReadyScript: 'onReady.js',
   }, scenario);
 });
-
-console.log(scenarios);
-
-if (!process.env.WAGTAIL_SESSIONID) {
-  throw new ReferenceError('Environment variable WAGTAIL_SESSIONID is not defined.');
-}
 
 module.exports = {
   id: 'wagtaildemo_master',
@@ -126,5 +119,5 @@ module.exports = {
   },
   engine: 'slimerjs',
   report: ['CI'],
-  debug: true
+  debug: false
 }
--- scripts/clickSelector.js
+++ scripts/clickSelector.js
@@ -8,7 +8,15 @@ module.exports = function(casper, scenario, vp) {
   onReady(casper, scenario, vp);
   casper.echo(`run clickSelector for ${scenario.label}, ${vp.name}, ${scenario.clickSelector}`);
 
-  casper.then(function(){
+  casper.then(function() {
+    var isMobile = ['phone', 'tablet_portrait'].indexOf(vp.name) !== -1;
+    var inSidebar = ['.dl-trigger', '.submenu-trigger', '#account-settings'].indexOf(scenario.clickSelector) !== -1;
+    if (isMobile && inSidebar) {
+      this.click('#nav-toggle');
+      this.wait(1000);
+    }
+
     this.click(scenario.clickSelector);
+    this.wait(1500);
   });
 };
