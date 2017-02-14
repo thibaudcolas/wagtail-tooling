966bf566bc272b39dae92df7480beef29ba8a52d
Thibaud Colas <thibaudcolas@gmail.com>
2017-02-14 13:19:17 +0200

    Update onReady.js

--- scripts/onReady.js
+++ scripts/onReady.js
@@ -7,12 +7,11 @@ module.exports = function(casper, scenario, vp) {
   casper.echo(`run onReady for ${scenario.label}, ${vp.name}`);
   casper.then(function() {
     this.evaluate(function() {
-      // Would be cool to prevent inadvertant interactions.
+      // Prevent inadvertent interaction with the page.
       var style = document.createElement('style');
       style.innerHTML = '* {' + [
         'cursor: none !important;',
         'pointer-events: none !important;',
-        'user-select: none !important; -moz-user-select: none !important;',
         'user-select: none !important; -moz-user-select: none !important;',
         'animation-delay: 0.01s !important; -webkit-animation-delay: 0.01s !important;',
         'animation-duration: 0.01s !important; -webkit-animation-duration: 0.01s !important;'
