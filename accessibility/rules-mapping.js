8402c144f3ba4c34e25841f341812ce8b816a8df
Thibaud Colas <thibaudcolas@gmail.com>
2019-04-10 11:41:20 +0100

    Start work on mapping from Axe to HTMLCS

---
+++ accessibility/rules-mapping.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const axe = require("axe-core");
+const allAxeRules = axe.getRules(["wcag2a", "wcag2aa", "wcag2aaa"]);
+
+const axeRules = allAxeRules.reduce((rules, rule) => {
+  rules[rule.ruleId] = rule.tags;
+  return rules;
+}, {});
+
+fs.writeFileSync(
+  "axe-rule-tags.json",
+  JSON.stringify(axeRules, null, 2),
+  "utf8",
+);
