4000b33c2afb53eb57368c6ee285c40f7c42fac5
Thibaud Colas <thibaudcolas@gmail.com>
2025-05-03 14:54:46 +0100

    Update wagtail-lighthouse-scores-2025_04_01.sql

--- http-archive/gaad-2025/wagtail-lighthouse-scores-2025_04_01.sql
+++ http-archive/gaad-2025/wagtail-lighthouse-scores-2025_04_01.sql
@@ -1,13 +1,15 @@
 #standardSQL
-# Lighthouse scores for Wagtail sites’ homepages, 2024-04-01 data
+# Lighthouse scores for Wagtail sites’ homepages, 2025-04-01 data
 SELECT
-  url,
-  JSON_EXTRACT_SCALAR(report, '$.categories.accessibility.score') AS accessibility,
-  JSON_EXTRACT_SCALAR(report, '$.categories.performance.score') AS performance,
-  JSON_EXTRACT_SCALAR(report, '$.categories.seo.score') AS seo,
-  JSON_EXTRACT_SCALAR(report, '$.categories.best-practices.score')  AS best_practices,
+  page,
+  rank,
+  JSON_EXTRACT_SCALAR(lighthouse, '$.categories.accessibility.score') AS accessibility,
+  JSON_EXTRACT_SCALAR(lighthouse, '$.categories.performance.score') AS performance,
+  JSON_EXTRACT_SCALAR(lighthouse, '$.categories.seo.score') AS seo,
+  JSON_EXTRACT_SCALAR(lighthouse, '$.categories.best-practices.score')  AS best_practices,
 FROM
-  `wagtail-analysis.wagtail_httparchive.2024_04_19_wagtail_crux_urls`
-JOIN
-  `wagtail-analysis.wagtail_httparchive.django-wagtail-lighthouse-2024_04_01_desktop`
-USING (url)
+  `wagtail-analysis.wagtail_httparchive.2025_04_01_django_wagtail_reports`
+WHERE
+  lighthouse IS NOT NULL
+  AND EXISTS (SELECT 1 FROM UNNEST(technologies) AS tech WHERE tech.technology = 'Wagtail')
+ORDER BY rank ASC
