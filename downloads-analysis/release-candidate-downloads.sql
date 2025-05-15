41d6763405f5544cdfc3db2421abfa93e575de3b
Thibaud Colas <thibaudcolas@gmail.com>
2025-05-15 13:09:53 +0100

    Update release-candidate-downloads.sql

--- downloads-analysis/release-candidate-downloads.sql
+++ downloads-analysis/release-candidate-downloads.sql
@@ -6,7 +6,7 @@ WITH downloads AS (
   WHERE dl.project = 'wagtail'
     -- Bytes processed target per query: 0.5 - 1GB (latest: 627 MB). Dry-run estimate: 500 GB
     -- 7.0
-    AND REGEXP_CONTAINS(dl.file.version, r'^(7\.0|6\.4($|\.))') AND dl.timestamp BETWEEN TIMESTAMP('2025-04-24T21:07:38.000Z') AND TIMESTAMP('2025-05-06T23:59:59.000Z')
+    AND REGEXP_CONTAINS(dl.file.version, r'^(7\.0|6\.4($|\.))') AND dl.timestamp BETWEEN TIMESTAMP('2025-04-24T21:07:38.000Z') AND TIMESTAMP('2025-05-06T15:41:29.000Z')
     -- 6.4
     -- AND REGEXP_CONTAINS(dl.file.version, r'^(6\.4|6\.3($|\.))') AND dl.timestamp BETWEEN TIMESTAMP('2025-01-20T18:49:20.000Z') AND TIMESTAMP('2025-02-03T17:09:01.000Z')
     -- 6.3
